01179da81f4ae1d79c43ce448aa9e6aa0d5076be
Hideki <61321130+Hideki515@users.noreply.github.com>
2024-06-26 18:05:43 -0300

    :sparkles: Aula 8 - Exercício com variáveis e tipos de dados

---
+++ Python/aula_8.py
@@ -0,0 +1,6 @@
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura)
