ec92002e33ab19facedb329926629313f7458393
Bruno Hideki Sato Misufara <brunohideki515@gmail.com>
2024-07-03 20:49:12 -0300

    :sparkles: Aula 30 - Fatiamento de strings e a função len

---
+++ Python/aula_30.py
@@ -0,0 +1,11 @@
+'''
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fateiamento [i:f:p] [::]
+Obs.: a função len retorna a quantidade de caracteres da str(String)
+'''
+variavel = 'Olá Mundo'
+print(f'{variavel[-8:-2]}')
+print(variavel[-1:-10:-1])
