18e5f6e235bfe48f41adc7cb869a90c7f24e8ffe
Bruno Hideki Sato Misufara <61321130+Hideki515@users.noreply.github.com>
2025-01-29 21:48:41 -0300

    ✨ Aula 43 - Operadores de atribuição com operadores aritméticos

---
+++ Python/aula_43.py
@@ -0,0 +1,17 @@
+"""
+Operadores de atribuição
+= += -= *= /= %= **= //=
+"""
+
+contador = 0
+
+while contador < 10:
+    print(contador)
+    contador += 1
+
+print("Acao concluida")
+
+valor = 10
+valor *= 2
+
+print(valor)
