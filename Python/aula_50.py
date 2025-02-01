85b10b9c4465cfd6403f6486dc154525faa1ade9
Bruno Hideki Sato Misufara <61321130+Hideki515@users.noreply.github.com>
2025-01-31 21:45:52 -0300

    ✨ Aula 50 - Calculadora - Parte 3

---
+++ Python/aula_50.py
@@ -0,0 +1,50 @@
+"""
+Calculadora com while
+"""
+
+while True:
+    numero_1 = input("Digite o primeiro número: ")
+    numero_2 = input("Digite o segundo número: ")
+    operador = input("Digite o operador: ")
+
+    numeros_validos = None
+
+    try:
+        numero_1_valido = float(numero_1)
+        numero_2_valido = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print("Digite um número válido.")
+        continue
+
+    operadores_permitidos = "+-*/"
+
+    if operador not in operadores_permitidos:
+        print("Digite um operador válido.")
+        continue
+
+    if len(operador) > 1:
+        print("Digite apenas um operador.")
+        continue
+
+    match operador:
+        case "+":
+            resultado = numero_1_valido + numero_2_valido
+            print(f"O resultado da soma é: {resultado}")
+        case "-":
+            resultado = numero_1_valido - numero_2_valido
+            print(f"O resultado da subtração é: {resultado}")
+        case "*":
+            resultado = numero_1_valido * numero_2_valido
+            print(f"O resultado da multiplicação é: {resultado}")
+        case "/":
+            resultado = numero_1_valido / numero_2_valido
+            print(f"O resultado da divisão é: {resultado}")
+
+    sair = input("Deseja sair? [s]im ou [n]ão: ").lower().startswith("s")
+
+    if sair is True:
+        break
